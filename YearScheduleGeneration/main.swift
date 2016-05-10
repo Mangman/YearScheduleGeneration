3ee2ac1a8d04d751fd42c87b481230e1c6d193aa
Stepan <biochoker@gmail.com>
2016-05-10 22:21:58 +0300

    Initial Commit

---
+++ YearScheduleGeneration/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YearScheduleGeneration
+//
+//  Created by admin on 10.05.16.
+//  Copyright © 2016 Stepan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
